8be8c2fc6e8f3b6f61e6786586100912fe330e9b
plektrud <andreas@thieltg.es>
2025-07-22 11:11:42 +0200

    Create clock.js

---
+++ js/clock.js
@@ -0,0 +1,8 @@
+  function updateClock() {
+    const now = new Date();
+    const date = now.toLocaleDateString('de-DE', { weekday: 'short', day: '2-digit', month: '2-digit', year: 'numeric' });
+    const time = now.toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+    document.getElementById('clock').textContent = date + ' ' + time;
+  }
+  setInterval(updateClock, 1000);
+  updateClock();
