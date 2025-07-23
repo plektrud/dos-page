9cf7ec7140bf28eb1b5abeb32b6228a92c1c8a3e
plektrud <andreas@thieltg.es>
2025-07-23 15:28:24 +0200

    Update loadcontent.js

--- js/loadcontent.js
+++ js/loadcontent.js
@@ -32,7 +32,7 @@ async function loadSection(name) {
 
     const storedName = localStorage.getItem('dosUserName');
     if (storedName && greeting) {
-      greeting.textContent = ${storedName};
+      greeting.textContent = storedName;
     }
 
     if (input && greeting) {
@@ -41,7 +41,7 @@ async function loadSection(name) {
           const name = input.value.trim();
           if (name !== "") {
             localStorage.setItem('dosUserName', name);
-            greeting.textContent = ${name}!;
+            greeting.textContent = name;
             updateCursorVisibility();
           }
         }
