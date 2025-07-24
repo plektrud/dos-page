24eb68345c52177681dc37e2004f1de8558d294f
plektrud <andreas@thieltg.es>
2025-07-24 07:22:05 +0200

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,8 @@
   function updateClock() {
     const now = new Date();
-    const date = now.toLocaleDateString('de-DE', { weekday: 'short', day: '2-digit', month: '2-digit', year: 'numeric' });
+    const date = now.toLocaleDateString('de-DE', { day: '2-digit', month: '2-digit', year: 'numeric' });
     const time = now.toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
-    document.getElementById('clock').textContent = date + ' ' + time;
+    document.getElementById('clock').textContent = date + ', ' + time;
   }
   setInterval(updateClock, 1000);
   updateClock();
